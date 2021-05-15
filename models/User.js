599e768bd582882816141b877c0dc6b28c8abfb4
aloysious x <alicia.todd.army@gmail.com>
2021-05-15 12:01:25 -0700

    binging in the mongoose!

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// defining image properties and thier datatypes :
+const imageOfTheDaySchema = new Schema({
+  title: String,
+  date: String,
+  explaination: String,
+  url: String,
+});
+
+// incldes the image schema 
+// user is an object that contains an array of book objects
+const userSchema = new Schema({
+  userName: String,
+  userEmail: String,
+  // array of activity Schema
+  favoriteImages: [imageOfTheDaySchema] 
+});
+
+//model of the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -3,19 +3,42 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const cors = require('cors');
 
+const cors = require('cors');
 app.use(cors());
+
 app.use(express.json());
+
+// require mongoose
+const mongoose = require('mongoose');
+
+// hey mongoose, connect to the database at localhost:27017
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const User = require('./models/User');
+
+const userProfile = new User({
+  userEmail: 'aloysiousx@gmail.com',
+  favoriteImages: [
+    {
+      title: 'howling wolf',
+      date: '05.20.2021',
+      explaination: 'stufffffffffffffffffffffff',
+      url: '',
+    }
+  ]
+});
+
+userProfile.save().then(() => console.log('successfully saved'));
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('🚀 Hello Fellow Space Cadet');
 });
 
 // app.get();
 
 
-
+console.log('❤️ Hello sPaCe CaDeTs welcome to the back-end! ❤️');
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
